d872abe16f70aa6daf9e8af47eb3a0e80c531a5d
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2019-03-18 02:07:57 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,15 +1,38 @@
 ;(function(){
 
 	var Game = function(canvasId) {
-		console.log("Hello, Game!");
+		var canvas = document.getElementById(canvasId);
+		var screen = canvas.getContext("2d");
+		var gameSize = {
+			x: canvas.width,
+			y: canvas.height			
+		};
+
+		var self = this;
+		var gameCycle = function() {
+			self.update();
+			self.draw(screen, gameSize);
+			requestAnimationFrame(tick);
+		}
+
+		gameCycle();
 	}
 
 	Game.prototype = {
 
+		update: function() {
+			console.log("update");
+		},
+
+		draw: function(screen, gameSize) {
+			//test render
+			screen.fillRect(150, 150, 32, 32);
+		}
+
 	}
 
 	window.omload = function() {
-		new Game("");
+		new Game("screen");
 	}
 
 })();
